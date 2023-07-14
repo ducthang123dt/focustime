e851a5f260f40fea208f6b850323b590d228c33f
ducthang123dt <ducthangdt0@gmail.com>
2023-07-14 16:34:44 +0700

    update

---
+++ App.js
@@ -0,0 +1,37 @@
+import {
+  Text,
+  View,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from 'react-native';
+import React, { useState } from 'react';
+import { colors } from './src/utils/colors';
+import { Focus } from './src/features/Focus';
+import { Timer } from './src/features/Timer';
+
+export default function App() {
+  const [currentSubject, setCurrentSubject] = useState(null);
+  return (
+    <SafeAreaView style={styles.container}>
+      {!currentSubject ? (
+        <Focus addSubject={setCurrentSubject} />
+      ) : (
+        <Timer 
+        focusSubject={currentSubject}
+        onTimerEnd={() => {}}
+        clearSubject={() => {setCurrentSubject(null)}}
+        />
+      )}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    backgroundColor: colors.darkYellow,
+  },
+});
---
+++ src/utils/colors.js
@@ -0,0 +1,6 @@
+export const colors = {
+  white: '#fff',
+  darkYellow: '#FFD93D',
+  black: '#000',
+  progressBar: '#5E84E2',
+};
